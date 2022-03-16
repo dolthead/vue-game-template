7d16be3276290a3eb9c44add3b860cd59393aa99
Todd H <github@simplybest.com>
2022-03-15 22:55:22 -0600

    page 1 done

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,68 +1,69 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+      <ion-toolbar color="primary">
+        <ion-title>Game</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="openAbout">
+            <ion-icon slot="icon-only" :icon="helpCircleOutline"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content class="ion-padding ion-justify-content-center">
+
+      <ion-card>
+        <ion-card-content>
+          <ion-img 
+            src="https://via.placeholder.com/400x300.png?text=Large+Icon"
+            alt="screenshot" 
+          ></ion-img>
+          <ion-grid>
+            <ion-row>
+              <ion-col>Stats</ion-col>
+              <ion-col>here</ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col>More stats</ion-col>
+              <ion-col>here</ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col>Final stats</ion-col>
+              <ion-col>here</ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-button fill="outline" expand="block" class="ion-margin-bottom">Quick tutorial</ion-button>
+      <ion-button fill="solid" expand="block">Start</ion-button>
     
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { 
+  IonContent, IonHeader, IonPage, IonTitle, IonIcon, IonButtons,
+  IonToolbar, IonCard, IonCardContent, IonImg, IonButton 
+} from '@ionic/vue';
+import { helpCircleOutline } from 'ionicons/icons';
 
-export default defineComponent({
-  name: 'HomePage',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
+const openAbout = () => {
+  console.log('open about')
+};
+
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
+  ion-card {
+    margin: 0 10vw 10vw 10vw;
+  }
+  ion-image {
+    width: 100%;
+  }
+  ion-col:last-child {
+    text-align: right;
+  }
 </style>
