ba118e4c068bb4187c07aec72d754eb037f901d1
Todd H <github@simplybest.com>
2022-03-16 14:48:04 -0600

    add firebase hosting

--- src/services/ShareService.ts
+++ src/services/ShareService.ts
@@ -1,20 +1,21 @@
 import { Share } from '@capacitor/share';
 import { Clipboard } from '@capacitor/clipboard';
 
+const url = 'https://ionicgametemplate.web.app';
+const msg = 'See if you can beat my score!';
+
 export const share = async (toastController: any) => {
   const canShare = await Share.canShare();
   if (canShare.value) {
     return Share.share({
       title: 'Game',
-      text: 'See if you can beat my score!',
-      url: 'https://sharetemplate.web.app',
+      text: msg,
+      url: url,
       dialogTitle: 'Share with buddies',
     });
   } else {
     await Clipboard.write({
-      string: `Game
-See if you can beat my score!
-https://sharetemplate.web.app`
+      string: `Game\n${msg}\n${url}`
     });
     const toast = await toastController.create({
       message: 'Copied to the clipboard',
